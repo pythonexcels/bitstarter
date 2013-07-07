81ce60288c4382448bd268fb95ea8ae16a2f2265
Dan White <dwhite618@gmail.com>
2013-07-07 03:55:22 +0000

    update web.js

--- web.js
+++ web.js
@@ -4,13 +4,15 @@ fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-    fs.readFileSync('/index.html', function (err, data) {
-	if (err) throw err;
-	var newdata = new Buffer(data,"utf-8");
-//	var newdata = new Buffer(1024);
-//	console.log(data);
-    });
-    response.send(newdata.tostring('utf-8'));
+    var data = fs.readFileSync('index.html')
+    var newdata = new Buffer(data,"utf-8");
+// 	if (err) throw err;
+// 	var newdata = new Buffer(data,"utf-8");
+// //	var newdata = new Buffer(1024);
+// //	console.log(data);
+//     });
+    // response.send(newdata.tostring('utf-8'));
+    response.send(newdata);
 });
 
 var port = process.env.PORT || 5000;
