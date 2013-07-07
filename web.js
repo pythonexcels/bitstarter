305bfe1f490c8874f630c911db2313450380aaad
Dan White <dwhite618@gmail.com>
2013-07-07 03:23:14 +0000

    buffer and readFileSync

--- web.js
+++ web.js
@@ -3,11 +3,17 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+    fs.readFileSync('/index.html', function (err, data) {
+	if (err) throw err;
+	var newdata = new Buffer(data,"utf-8");
+//	var newdata = new Buffer(1024);
+//	console.log(data);
+    });
+    response.send(newdata.tostring('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-  console.log("Listening on " + port);
+    console.log("Listening on " + port);
 });
 
