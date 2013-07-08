ece55687afbec21ed3bc1da14f3f4838df93a561
Dan White <dwhite618@gmail.com>
2013-07-08 03:12:59 +0000

    added /index.html

--- web.js
+++ web.js
@@ -16,6 +16,13 @@ app.get('/', function(request, response) {
     response.send(fs.readFileSync('index.html',"utf-8"));
 });
 
+app.get('/index.html', function(request, response) {
+    // var data = fs.readFileSync('index.html',"utf-8")
+    // var newdata = new Buffer(data,"utf-8");
+    // response.send(newdata);
+    response.send(fs.readFileSync('index.html',"utf-8"));
+});
+
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
     console.log("Listening on " + port);
