85a8afcd296c3f806561614655b76597c3e90eea
Dan White <dwhite618@gmail.com>
2013-07-08 02:26:12 +0000

    update

--- web.js
+++ web.js
@@ -10,7 +10,7 @@ var app = express.createServer(express.logger());
 
 
 app.get('/', function(request, response) {
-    var data = fs.readFileSync('index.html')
+    var data = fs.readFileSync('index.html',"utf-8")
     var newdata = new Buffer(data,"utf-8");
     response.send(newdata);
 });
